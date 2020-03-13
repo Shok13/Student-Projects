d0798e10eb890e4bba801fab1776eb682a11f117
Shok13 <duchamp2619@mail.ru>
2020-03-13 13:03:15 +0300

    Initial commit

---
+++ Lab#7/SortingMethods/SortingMethods/Program.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.IO;
+
+namespace SortingMethods
+{
+    class Program
+    {
+        static void Swap(ref int first, ref int second)
+        {
+            int tmp = first;
+            first = second;
+            second = tmp;
+        }
+        static void SelectionSort(ref int[] array, int left, int right)
+        {
+            long numberOfComparisons = 0;
+            int numberOfPermutations = 0;
+            var start = DateTime.Now;
+            for (int i = left; i < right; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j <= right; j++)
+                {
+                    numberOfComparisons++;
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+
+                }
+                numberOfPermutations++;
+                Swap(ref array[i], ref array[min]);
+            }
+            var end = DateTime.Now;
+            var interval = end - start;
+            ShowResult(numberOfComparisons, numberOfPermutations, interval);
+            WriteInfo(array);
+        }
+        static void InsertionSort(ref int[] array, int left, int right)
+        {
+            long numberOfComparisons = 0;
+            int numberOfPermutations = 0;
+            var start = DateTime.Now;
+
+            for (int i = left + 1; i <= right; i++)
+            {
+                int j = i;
+                int tmp = array[i];
+
+                while (j > left && tmp < array[j - 1])
+                {
+                    numberOfComparisons++;
+                    array[j] = array[j - 1];
+                    j--;
+                }
+
+                array[j] = tmp;
+                numberOfPermutations++;
+            }
+
+            var end = DateTime.Now;
+            var interval = end - start;
+            ShowResult(numberOfComparisons, numberOfPermutations, interval);
+            WriteInfo(array);
+        }
+        static void BubbleSort(ref int[] array, int left, int right)
+        {
+            long numberOfComparisons = 0;
+            int numberOfPermutations = 0;
+            var start = DateTime.Now;
+
+            for (int i = left; i < right; i++)
+            {
+                for (int j = right; j > i; j--)
+                {
+                    numberOfComparisons++;
+                    if (array[j - 1] > array[j])
+                    {
+                        numberOfPermutations++;
+                        Swap(ref array[j - 1], ref array[j]);
+                    }
+                }
+            }
+
+            var end = DateTime.Now;
+            var interval = end - start;
+            ShowResult(numberOfComparisons, numberOfPermutations, interval);
+            WriteInfo(array);
+        }
+        static void ShakerSort(ref int[] array, int left, int right)
+        {
+            long numberOfComparisons = 0;
+            long numberOfPermutations = 0;
+            var start = DateTime.Now;
+
+            do
+            {
+                for (int i = left; i < right; i++)
+                {
+                    numberOfComparisons++;
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(ref array[i], ref array[i + 1]);
+                        numberOfPermutations++;
+                    }
+                }
+
+                right--;
+
+                for (int i = right; i > left; i--)
+                {
+                    numberOfComparisons++;
+                    if (array[i] < array[i - 1])
+                    {
+                        Swap(ref array[i], ref array[i - 1]);
+                        numberOfPermutations++;
+                    }
+                }
+
+                left++;
+            }
+            while (left <= right);
+
+            var end = DateTime.Now;
+            var interval = end - start;
+            ShowResult(numberOfComparisons, numberOfPermutations, interval);
+            WriteInfo(array);
+        }
+        static void ShellSort(ref int[] array, int left, int right)
+        {
+            long numberOfComparisons = 0;
+            int numberOfPermutations = 0;
+            var start = DateTime.Now;
+
+            int[] H = { 57, 23, 10, 4, 1 };
+            int HN = H.Length;
+
+            foreach (int step in H)
+            {
+                for (int i = left + step; i <= right; i++)
+                {
+                    int j = i;
+                    int tmp = array[i];
+
+                    while (j >= left + step && tmp < array[j - step])
+                    {
+                        numberOfComparisons++;
+                        array[j] = array[j - step];
+                        j -= step;
+                    }
+                    array[j] = tmp;
+                    numberOfPermutations++;
+                }
+            }
+
+            var end = DateTime.Now;
+            var interval = end - start;
+            ShowResult(numberOfComparisons, numberOfPermutations, interval);
+            WriteInfo(array);
+        }
+        static void ShowResult(long comparisons, long permutations, TimeSpan interval)
+        {
+            Console.WriteLine($"Время работы - {interval.ToString(),21}\n"
+                             + $"Количество сравнений:{comparisons,15}\n"
+                             + $"Количество перестановок:{permutations,12}\n");
+        }
+        static void WriteInfo(int[] array)
+        {
+            string path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()).ToString()).ToString() + "\\sorted.dat";
+            using (var sw = new StreamWriter(path, append: true))
+            {
+                foreach (int x in array)
+                {
+                    sw.Write(x+" ");
+                }
+                sw.WriteLine();
+            }
+        }
+        static void CheckArray(int[] array)
+        {
+
+            foreach (int x in array)
+            {
+                Console.Write($"{x},");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            string path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()).ToString()).ToString() + "\\sorted.dat";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            var arrayLength = 40000;
+            var randomArray = new int[arrayLength];
+            var arrayToSort = new int[arrayLength];
+            var rnd = new Random();
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = rnd.Next(1000);
+            }
+
+            Array.Copy(randomArray, arrayToSort, randomArray.Length);
+            #region SelectionSort
+            Console.WriteLine("Сортировка выбором массива, заполненного случайными числами");
+            SelectionSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Console.WriteLine("Сортировка выбором массива, заполненного числами в порядке возрастания");
+            SelectionSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Array.Reverse(arrayToSort);
+            Console.WriteLine("Сортировка выбором массива, заполненного числами в порядке убывания");
+            SelectionSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+            #endregion
+
+
+            Array.Copy(randomArray, arrayToSort, randomArray.Length);
+            #region InsertionSort
+            Console.WriteLine("Сортировка вставками массива, заполненного случайными числами");
+            InsertionSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Console.WriteLine("Сортировка вставками массива, заполненного числами в порядке возрастания");
+            InsertionSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Array.Reverse(arrayToSort);
+            Console.WriteLine("Сортировка вставками массива, заполненного числами в порядке убывания");
+            InsertionSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+            #endregion
+
+
+            Array.Copy(randomArray, arrayToSort, randomArray.Length);
+            #region BubbleSort
+            Console.WriteLine("Сортировка пузырьком массива, заполненного случайными числами");
+            BubbleSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Console.WriteLine("Сортировка пузырьком массива, заполненного числами в порядке возрастания");
+            BubbleSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Array.Reverse(arrayToSort);
+            Console.WriteLine("Сортировка пузырьком массива, заполненного числами в порядке убывания");
+            BubbleSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+            #endregion
+
+
+            Array.Copy(randomArray, arrayToSort, randomArray.Length);
+            #region ShellSort
+            Console.WriteLine("Сортировка массива алгоритмом Шелла, заполненного случайными числами");
+            ShellSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Console.WriteLine("Сортировка массива алгоритмом Шелла, заполненного числами в порядке возрастания");
+            ShellSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Array.Reverse(arrayToSort);
+            Console.WriteLine("Сортировка массива алгоритмом Шелла, заполненного числами в порядке убывания");
+            ShellSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+            #endregion
+
+
+            Array.Copy(randomArray, arrayToSort, randomArray.Length);
+            #region ShakerSort
+            Console.WriteLine("Сортировка шейкером массива, заполненного случайными числами");
+            ShakerSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Console.WriteLine("Сортировка шейкером массива, заполненного числами в порядке возрастания");
+            ShakerSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+
+            Array.Reverse(arrayToSort);
+            Console.WriteLine("Сортировка шейкером массива, заполненного числами в порядке убывания");
+            ShakerSort(ref arrayToSort, 0, arrayToSort.Length - 1);
+            #endregion
+        }
+    }
+}
---
+++ Lab#7/Tab_Sort/Tab_Sort/Program.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.IO;
+
+namespace Tab
+{
+    class Program
+    {
+        enum ProductType
+        {
+            О,
+            К
+        }
+        enum Action
+        {
+            ADD,
+            DELETE,
+            UPDATE
+        }
+        struct Product
+        {
+            private string Name;
+            public string name
+            {
+                get
+                {
+                    return Name;
+                }
+                set
+                {
+                    Name = value;
+                }
+            }
+            private ProductType Type;
+            public ProductType type => Type;
+
+            private decimal Price;
+            public decimal price => Price;
+
+            private uint Amount;
+            public uint amount => Amount;
+
+            public Product(string name, ProductType type, decimal price, uint amount)
+            {
+                Name = name;
+                Type = type;
+                Price = price;
+                Amount = amount;
+            }
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"|{Name,-23}|{Type}\t\t|{Price,-23:0.00}|{Amount,-14}|");
+            }
+            public void WriteInfo(int length, string path, Product[] products)
+            {
+
+                using (StreamWriter sw = new StreamWriter(path, append: false))
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        sw.WriteLine($"|{products[i].name}|{products[i].type}|{products[i].price:0.00}|{products[i].amount}|");
+                    }
+                }
+                Console.WriteLine("Данные сохранены...");
+            }
+            public static void ReadInfo(ref Product[] products, string path)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        while (sr.Peek() != -1)
+                        {
+                            string[] data = sr.ReadLine().Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                            if (data.Length == 4)
+                            {
+                                AddNote(ref products, data);
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Файл не обнаружен...\n");
+                }
+            }
+        }
+        struct Log
+        {
+            public DateTime time;
+            public Action action;
+            public string name;
+
+            public void DisplayLog()
+            {
+                switch (action)
+                {
+                    case Action.ADD:
+                        Console.WriteLine($"{time.ToLongTimeString()} - Добавлена запись \"{name}\"");
+                        break;
+                    case Action.DELETE:
+                        Console.WriteLine($"{time.ToLongTimeString()} - Удалена   запись \"{name}\"");
+                        break;
+                    case Action.UPDATE:
+                        Console.WriteLine($"{time.ToLongTimeString()} - Обновлена запись \"{name}\"");
+                        break;
+                }
+            }
+        }
+
+        static void CalcIdleTime(DateTime dt1, TimeSpan old_idle_time, out TimeSpan idle_time)
+        {
+            DateTime dt2 = DateTime.Now;
+            TimeSpan time = dt2 - dt1;
+            if (time > old_idle_time)
+            {
+                idle_time = time;
+            }
+            else
+            {
+                idle_time = old_idle_time;
+            }
+        }
+
+        static int GetNumber(int left_limit, uint right_limit)
+        {
+            int select = Int32.MinValue;
+            while (!(select >= left_limit && select <= right_limit))
+            {
+                Console.Write($"Введите значение от {left_limit} до {right_limit}:");
+                try
+                {
+                    select = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Попробуйте заново!");
+                }
+            }
+            return select;
+        }
+        static ProductType GetTypeProduct()
+        {
+        Start:
+            string input = Console.ReadLine();
+            char type = input[0];
+
+            if (type == 'O' || type == 'o' ||
+                type == 'О' || type == 'о')
+            {
+                return ProductType.О;
+            }
+            else if (type == 'K' || type == 'k' || type == 'К' || type == 'к')
+            {
+                return ProductType.К;
+            }
+            else
+            {
+                goto Start;
+            }
+        }
+        static ProductType GetTypeProduct(char type)
+        {
+            if (type == 'O' || type == 'o' ||
+                type == 'О' || type == 'о')
+            {
+                return ProductType.О;
+            }
+            else if (type == 'K' || type == 'k' ||
+                     type == 'К' || type == 'к')
+            {
+                return ProductType.К;
+            }
+            else
+            {
+                return ProductType.К;
+            }
+        }
+        static decimal GetPrice(decimal left_limit, decimal right_limit)
+        {
+            decimal price = Decimal.MinValue;
+            while (!(price >= left_limit && price <= right_limit))
+            {
+                Console.Write($"Введите значение от {left_limit} до {right_limit}:");
+                try
+                {
+                    price = Convert.ToDecimal(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Попробуйте заново!");
+                }
+            }
+            return price;
+        }
+        const int Show_Tab = 1;
+        const int Add_Note = 2;
+        const int Delete_Note = 3;
+        const int Update_Note = 4;
+        const int Search_Notes = 5;
+        const int Sort_ = 6;
+        const int Show_Log = 7;
+        const int Exit = 8;
+        static void ShowMenu()
+        {
+            Console.WriteLine($"{Show_Tab} – Просмотр таблицы\n"
+                            + $"{Add_Note} – Добавить запись\n"
+                            + $"{Delete_Note} – Удалить запись\n"
+                            + $"{Update_Note} – Обновить запись\n"
+                            + $"{Search_Notes} – Поиск записей\n"
+                            + $"{Sort_} - Сортировать по убыванию цены\n"
+                            + $"{Show_Log} – Просмотреть лог\n"
+                            + $"{Exit} - Выход");
+        }
+        static void ShowTab(Product[] products)
+        {
+            string s = "--------------------------------------------------------------------------------";
+            Console.WriteLine(s);
+            Console.WriteLine("|Прайс лист\t\t\t\t\t\t\t\t       |");
+            Console.WriteLine(s);
+            Console.WriteLine("|Наименование товара \t|Тип товара \t|Цена за 1 шт (грн) \t|Количество    |");
+            Console.WriteLine(s);
+            for (int i = 0; i < products.Length; i++)
+            {
+                products[i].DisplayInfo();
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("|Перечисляемый тип: О - оргтехника, К – канцтовары\t\t\t       |");
+            Console.WriteLine(s);
+        }
+        static void ShowLog(Log[] logs, TimeSpan idle_time)
+        {
+            for (int i = 0; i < logs.Length; i++)
+            {
+                if (logs[i].name != null)
+                {
+                    logs[i].DisplayLog();
+                }
+            }
+            Console.WriteLine($"\n{idle_time.Hours:00}:{idle_time.Minutes:00}:{idle_time.Seconds:00} - Самый долгий период бездействия пользователя");
+        }
+        static void AddNote(ref Product[] products, ref Log[] logs, ref int cnt)
+        {
+            Array.Resize(ref products, products.Length + 1);
+            int last_pos = products.Length - 1;
+
+            Console.Write("Введите наименование товара:");
+            string name = (Console.ReadLine());
+
+            Console.WriteLine("Укажите его тип (Оргтехника - O, Канцтовары - K)");
+            var type = GetTypeProduct();
+
+            Console.WriteLine("Укажите цену за 1шт (грн)");
+            var price = GetPrice(0, Decimal.MaxValue);
+
+            Console.WriteLine("Укажите количество");
+            var amount = (uint)GetNumber(0, UInt32.MaxValue);
+
+            products[last_pos] = new Product(name, type, price, amount);
+            AddLog(ref logs, DateTime.Now, Action.ADD, name, ref cnt);
+        }
+        static void AddNote(ref Product[] products, string[] data)
+        {
+            bool match = false;
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (data[0] == products[i].name)
+                {
+                    match = true;
+                }
+            }
+            if (!match)
+            {
+                Array.Resize(ref products, products.Length + 1);
+                products[products.Length - 1] = new Product(data[0], GetTypeProduct(Convert.ToChar(data[1])),
+                                          Convert.ToDecimal(data[2]), Convert.ToUInt32(data[3]));
+            }
+        }
+        static void DeleteNote(ref Product[] products, ref Log[] logs, ref int cnt)
+        {
+            Console.WriteLine("Укажите номер записи, которую хотите удалить");
+            int number = GetNumber(0, (uint)products.Length - 1);
+            AddLog(ref logs, DateTime.Now, Action.DELETE, products[number].name, ref cnt);
+            for (int i = number; i < products.Length - 1; i++)
+            {
+                products[i] = products[i + 1];
+            }
+            Array.Resize(ref products, products.Length - 1);
+        }
+        static void UpdateNote(ref Product[] products, ref Log[] logs, ref int cnt)
+        {
+            Console.WriteLine("Укажите номер записи, которую хотите обновить");
+            int number = GetNumber(0, (uint)products.Length - 1);
+            AddLog(ref logs, DateTime.Now, Action.UPDATE, products[number].name, ref cnt);
+            Console.Write("Введите наименование товара:");
+            string name = (Console.ReadLine());
+
+            Console.WriteLine("Укажите его тип (Оргтехника - O, Канцтовары - K)");
+            var type = GetTypeProduct();
+
+            Console.WriteLine("Укажите цену за 1шт (грн)");
+            var price = GetPrice(0, Decimal.MaxValue);
+
+            Console.WriteLine("Укажите количество");
+            var amount = (uint)GetNumber(0, UInt32.MaxValue);
+            products[number] = new Product(name, type, price, amount);
+        }
+        static void SearchNotes(Product[] products)
+        {
+            Console.WriteLine("Введите число, больше которого будет отображаться количество товаров");
+            int amount = Int32.MinValue;
+            amount = GetNumber(0, Int32.MaxValue);
+            string s = "--------------------------------------------------------------------------------";
+            Console.WriteLine(s);
+            Console.WriteLine("|Прайс лист\t\t\t\t\t\t\t\t       |");
+            Console.WriteLine(s);
+            Console.WriteLine("|Наименование товара \t|Тип товара \t|Цена за 1 шт (грн) \t|Количество    |");
+            Console.WriteLine(s);
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i].amount > amount)
+                {
+                    products[i].DisplayInfo();
+                    Console.WriteLine(s);
+                }
+            }
+            Console.WriteLine("|Перечисляемый тип: О - оргтехника, К – канцтовары\t\t\t       |");
+            Console.WriteLine(s);
+        }
+        static void Sort(Product[] products, int left, int right)
+        {
+            for (int i = left; i < right; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j <= right; j++)
+                {
+                    if (products[j].price < products[min].price)
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(ref products[i], ref products[min]); 
+            }
+        }
+        static void Swap(ref Product products_first, ref Product products_second)
+        {
+            var name = products_first.name;
+            var type = products_first.type;
+            var price = products_first.price;
+            var amount = products_first.amount;
+
+            products_first = new Product(products_second.name, products_second.type, products_second.price, products_second.amount);
+            products_second = new Product(name, type, price, amount);
+        }
+        static void AddLog(ref Log[] logs, DateTime time, Action action, string name, ref int cnt)
+        {
+            if (cnt > 49)
+            {
+                for (int i = 0; i < logs.Length - 1; i++)
+                {
+                    logs[i] = logs[i + 1];
+                }
+                cnt = 49;
+            }
+            logs[cnt].time = time;
+            logs[cnt].action = action;
+            logs[cnt].name = name;
+
+            cnt++;
+        }
+        static void Main(string[] args)
+        {
+            string path = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()).ToString() + "\\lab.dat";
+            Product[] products = new Product[3];
+            Log[] logs = new Log[50];
+
+            products[0] = new Product("Папка", ProductType.К, 4.75m, 400);
+            products[1] = new Product("Бумага А4(пачка)", ProductType.К, 45.90m, 100);
+            products[2] = new Product("Калькулятор", ProductType.О, 411m, 10);
+
+            Product.ReadInfo(ref products, path);
+
+            int cnt = 0;
+
+            int select = 0;
+            bool exit = false;
+            TimeSpan idle_time = TimeSpan.Zero;
+            DateTime time = DateTime.Now;
+            while (!exit)
+            {
+                switch (select)
+                {
+                    case Show_Tab:
+                        ShowTab(products);
+                        CalcIdleTime(time, idle_time, out idle_time);
+                        time = DateTime.Now;
+                        ShowMenu();
+                        select = GetNumber(Show_Tab, Exit);
+                        break;
+                    case Add_Note:
+                        AddNote(ref products, ref logs, ref cnt);
+                        CalcIdleTime(time, idle_time, out idle_time);
+                        time = DateTime.Now;
+                        ShowMenu();
+                        select = GetNumber(Show_Tab, Exit);
+                        break;
+                    case Delete_Note:
+                        DeleteNote(ref products, ref logs, ref cnt);
+                        CalcIdleTime(time, idle_time, out idle_time);
+                        time = DateTime.Now;
+                        ShowMenu();
+                        select = GetNumber(Show_Tab, Exit);
+                        break;
+                    case Update_Note:
+                        UpdateNote(ref products, ref logs, ref cnt);
+                        CalcIdleTime(time, idle_time, out idle_time);
+                        time = DateTime.Now;
+                        ShowMenu();
+                        select = GetNumber(Show_Tab, Exit);
+                        break;
+                    case Search_Notes:
+                        SearchNotes(products);
+                        CalcIdleTime(time, idle_time, out idle_time);
+                        time = DateTime.Now;
+                        ShowMenu();
+                        select = GetNumber(Show_Tab, Exit);
+                        break;
+                    case Show_Log:
+                        Console.Clear();
+                        ShowLog(logs, idle_time);
+                        CalcIdleTime(time, idle_time, out idle_time);
+                        time = DateTime.Now;
+                        ShowMenu();
+                        select = GetNumber(Show_Tab, Exit);
+                        break;
+                    case Sort_:
+                        Sort(products, 0, products.Length - 1);
+                        Console.WriteLine("Сортировка завершена.");
+                        CalcIdleTime(time, idle_time, out idle_time);
+                        time = DateTime.Now;
+                        ShowMenu();
+                        select = GetNumber(Show_Tab, Exit);
+                        break;
+                    case Exit:
+                        products[0].WriteInfo(products.Length, path, products);
+                        exit = true;
+                        break;
+                    default:
+                        CalcIdleTime(time, idle_time, out idle_time);
+                        time = DateTime.Now;
+                        ShowMenu();
+                        select = GetNumber(Show_Tab, Exit);
+                        break;
+                }
+            }
+        }
+    }
+}
